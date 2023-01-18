f5084fffd88bf06da59e4b494bedb3f9cee71439
c1570 <c1570@users.noreply.github.com>
2023-01-18 16:11:00 +0100

    both MCUs running and wired

--- demo/ntc-run.ts
+++ demo/ntc-run.ts
@@ -8,7 +8,7 @@ import { GDBTCPServer } from '../src/gdb/gdb-tcp-server';
 // Create an array with the compiled code of blink
 // Execute the instructions from this array, one by one.
 const hex1 = fs.readFileSync('/home/mayne/project/PicoDVI/software/build/apps/ntc64_main/ntc64_main.hex', 'utf-8');
-const hex2 = fs.readFileSync('hello_uart.hex', 'utf-8');
+const hex2 = fs.readFileSync('/home/mayne/project/PicoDVI/software/build/apps/ntc64_vic/ntc64_vic.hex', 'utf-8');
 
 const mcu1 = new RP2040();
 const mcu2 = new RP2040();
@@ -38,30 +38,27 @@ export enum GPIOPinState {
 }
 */
 
-let pin_state: GPIOPinState[] = [0,0,0,0,0,0,0,0,0,0];
+let pin_state: GPIOPinState[][] = [[3,3,3,3,3,3,3,3,3,3], [3,3,3,3,3,3,3,3,3,3]]; // all in input pullup mode
 let pin_name: string[] = ["clock", "d0", "d1", "d2", "d3", "d4", "d5", "d6", "d7", "vic_ack"];
 let pin_id: string[] = ["!", "§", "$", "%", "&", "/", "(", ")", "=", "*"];
 let vcd_file = fs.createWriteStream('/tmp/ntc64rp2040.vcd', {});
 
-function pinListener(pin: number) {
+function pinListener(mcu_id: number, pin: number) {
   return (state: GPIOPinState, oldState: GPIOPinState) => {
-    let v: number = (state===GPIOPinState.Low)?0:1;
-    vcd_file.write(`#${mcu1.core.cycles} ${v}${pin_id[pin]}\n`);
-    pin_state[pin] = state;
+    pin_state[mcu_id][pin] = state;
+    let v: number = ((pin_state[0][pin]===0)||(pin_state[1][pin]===0))?0:1; //TODO handle collisions/high
+
     mcu1.gpio[pin+2].setInputValue((v==1)?true:false);
+    mcu2.gpio[pin+2].setInputValue((v==1)?true:false);
+
+    vcd_file.write(`#${mcu1.core.cycles} ${v}${pin_id[pin]}\n`);
   };
 }
 
-mcu1.gpio[2].addListener(pinListener(0));
-mcu1.gpio[3].addListener(pinListener(1));
-mcu1.gpio[4].addListener(pinListener(2));
-mcu1.gpio[5].addListener(pinListener(3));
-mcu1.gpio[6].addListener(pinListener(4));
-mcu1.gpio[7].addListener(pinListener(5));
-mcu1.gpio[8].addListener(pinListener(6));
-mcu1.gpio[9].addListener(pinListener(7));
-mcu1.gpio[10].addListener(pinListener(8));
-mcu1.gpio[11].addListener(pinListener(9));
+for(let i = 0; i < pin_name.length; i++) {
+  mcu1.gpio[i+2].addListener(pinListener(0, i));
+  mcu2.gpio[i+2].addListener(pinListener(1, i));
+}
 
 mcu1.core.PC = 0x10000000;
 mcu2.core.PC = 0x10000000;
