c1ff08a127d1d345b22abedc69cfa6a6fbda9dfc
c1570 <c1570@users.noreply.github.com>
2023-01-30 19:30:33 +0100

    fix systick.cvr

--- demo/emulator-run.ts
+++ demo/emulator-run.ts
@@ -6,7 +6,7 @@ import { GDBTCPServer } from '../src/gdb/gdb-tcp-server';
 
 // Create an array with the compiled code of blink
 // Execute the instructions from this array, one by one.
-const hex = fs.readFileSync('hello_uart.hex', 'utf-8');
+const hex = fs.readFileSync('demo/systick/build/systick.hex', 'utf-8');
 const mcu = new RP2040();
 mcu.loadBootrom(bootromB1);
 loadHex(hex, mcu.flash, 0x10000000);
