54a7ed9134b0a5e9893b70c7ba6b4fcaa5802784
c1570 <c1570@users.noreply.github.com>
2024-12-28 18:54:19 +0100

    riscv test files

--- demo/emulator-run.ts
+++ demo/emulator-run.ts
@@ -6,14 +6,10 @@ import { GDBTCPServer } from '../src/gdb/gdb-tcp-server';
 
 const homedir = require('os').homedir();
 
-// Create an array with the compiled code of blink
-// Execute the instructions from this array, one by one.
-//const hex = fs.readFileSync(homedir + '/project/PicoDVI/software/build/apps/ntc64_main/ntc64_main.hex', 'utf-8');
-//const hex = fs.readFileSync(homedir + '/project/pico-examples/build/multicore/hello_multicore/hello_multicore.hex', 'utf-8');
-const hex = fs.readFileSync('hello_uart.hex', 'utf-8');
+const hex = fs.readFileSync('demo/riscv_blink/blink_simple.hex', 'utf-8');
 const mcu = new RP2040();
 mcu.loadBootrom(bootromB1);
-loadHex(hex, mcu.flash, 0x10000000);
+loadHex(hex, mcu.flash, 0x20000000);
 
 //const gdbServer = new GDBTCPServer(mcu, 3333);
 //console.log(`RP2040 GDB Server ready! Listening on port ${gdbServer.port}`);
@@ -22,8 +18,8 @@ mcu.uart[0].onByte = (value) => {
   process.stdout.write(new Uint8Array([value]));
 };
 
-mcu.core0.PC = 0x10000000;
-mcu.core1.PC = 0x10000000;
+mcu.core0.PC = 0x20000000;
+mcu.core1.PC = 0x20000000;
 mcu.core1.waiting = true;
 while(1) {
   mcu.step();
